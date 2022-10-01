ce787336aab1dbf0432b87c25fbaa592146846f0
fajar abdillah ahmad <fajardillah25@gmail.com>
2022-10-01 20:25:03 +0700

    LombaApk

--- src/LatihanMembuatTodolist/TodolistApp/LombaApp.java
+++ src/LatihanMembuatTodolist/TodolistApp/LombaApp.java
@@ -1,2 +1,19 @@
-package LatihanMembuatTodolist.TodolistApp;public class LombaApp {
+package LatihanMembuatTodolist.TodolistApp;
+
+import LatihanMembuatTodolist.Repository.LombaRepository;
+import LatihanMembuatTodolist.Repository.LombaRepositoryImpl;
+import LatihanMembuatTodolist.Service.ServiceLomba;
+import LatihanMembuatTodolist.Service.ServiceLombaImpl;
+import LatihanMembuatTodolist.View.ViewLomba;
+
+public class LombaApp {
+
+  public static void main(String[] args) {
+    LombaRepository lombaRepository = new LombaRepositoryImpl();
+    ServiceLomba serviceLomba = new ServiceLombaImpl(lombaRepository);
+    ViewLomba viewLomba = new ViewLomba(serviceLomba);
+
+    viewLomba.menuLomba();
+  }
+
 }
